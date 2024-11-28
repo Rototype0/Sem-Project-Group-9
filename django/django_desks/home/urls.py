5d2a2d0a0f9fc8d67b52511521d3eaea04231336
Rototype <67031469+Rototype0@users.noreply.github.com>
2024-11-28 03:02:44 +0200

    Added some basic functionality as well as all necessary pages with a basic layout for the dashboard

--- django/django_desks/home/urls.py
+++ django/django_desks/home/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name = 'dashboard')
+    path('', views.dashboard, name = 'dashboard'),
+    path('charts', views.charts, name = 'charts'),
+    path('modes', views.modes, name = 'modes'),
+    path('profile', views.profile, name = 'profile')
 ]
