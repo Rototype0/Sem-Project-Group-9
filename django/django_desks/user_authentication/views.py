c60441be7d7b399235a90079d5d430fc31d311b0
filipsima <fisim23@student.sdu.dk>
2024-12-02 01:00:42 +0100

    Registration form in the early stages

--- django/django_desks/user_authentication/views.py
+++ django/django_desks/user_authentication/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+
 
 def login_user(request):
     if request.method == "POST":
@@ -22,3 +24,22 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You were Logged Out"))
     return redirect('dashboard')
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Registration successful!"))
+            return redirect('dashboard')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'authenticate/register_user.html', {
+        'form':form,
+    })
