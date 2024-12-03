403ab27df474844a6b57cc8f2f5b648febdf711f
Ivan <newgenverba@gmail.com>
2024-12-03 10:35:42 +0200

    models updated
    position related ones are still worked on, just a prototype for now

--- django/django_desks/home/models.py
+++ django/django_desks/home/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Config (models.Model):
+    name = models.CharField(max_length=100)
+    manufacturer = models.CharField(max_length=100)
+
+class State (models.Model):
+    position_mm = models.IntegerField (null=False, blank=False)
+    speed_mms = models.IntegerField()
+    status = models.CharField(max_length=100) #not sure about this
+    isPositionLost = models.BooleanField(default=False)
+    isOverloadProtectionUp = models.BooleanField(default=False)
+    isOverloadProtectionDown = models.BooleanField(default=False)
+    isAntiCollision = models.BooleanField(default=False)
+
+
+class DeskUser (models.Model):
+    username = models.CharField (max_length = 100, null=False, blank=False)
+    height = models.IntegerField (null=False, blank=False) #position_mm?
+    fav_height_set = models.OneToOneField(
+        'FavouritePositionsSet', 
+        on_delete=models.CASCADE, 
+        related_name='user'
+    )
+
+class FavouritePositionSets (models.Model):
+    pass
+
+class FavPosition (models.Model):
+    desk_height = models.IntegerField (null=False, blank=False)
+    fav_position_set = models.ForeignKey(
+        FavouritePositionsSet, 
+        on_delete=models.CASCADE, 
+        related_name='fav_positions'
+    )
