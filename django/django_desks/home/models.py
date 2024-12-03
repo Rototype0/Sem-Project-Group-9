2da68a4d94a1e0375bbf2bc56fb998a14b24ec5b
Rokas <rokutis.norbutas@gmail.com>
2024-12-03 09:34:56 +0200

    Played around with some variants, made to look pretty will work on functionalioty some more in the near future

--- django/django_desks/home/models.py
+++ django/django_desks/home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class HeightProfile(models.Model):
+    name = models.CharField(max_length = 100)
+    height = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+class Desk(models.Model):
+    name = models.CharField(max_length = 100)
+    height = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.name
+
--- django/django_desks/home/views.py
+++ django/django_desks/home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Desk, HeightProfile
 
 # Create your views here.
 
@@ -13,3 +15,10 @@ def modes(request):
 
 def profile(request):
     return render(request, 'home/profile.html')
+
+def applyHeightProfile(request, desk_id, profile_id):
+    desk = get_object_or_404(Desk, id = desk_id)
+    profile = get_object_or_404(HeightProfile, id = profile_id)
+    desk.height = profile.height
+    desk.save()
+    return render('home/dashboard.html', {'profile': profile})
