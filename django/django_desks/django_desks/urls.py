c48703dedc6c9f59b24b3537d420099522424f11
Drago361 <alexftvyt@gmail.com>
2024-12-23 18:07:02 +0200

    final edit

--- django/django_desks/django_desks/urls.py
+++ django/django_desks/django_desks/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from desk_controller.views import fetch_desks, desk_info, desk_state_update
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', fetch_desks, name='fetch_desks'),
+    path('', fetch_desks, name='fetch_desks'),
     path('home/<int:desk_id>/', desk_info, name='desk'),
     path('home/<int:desk_id>/state/', desk_state_update, name='desk_category_update'),
     path('user_authentication/', include('django.contrib.auth.urls')),
