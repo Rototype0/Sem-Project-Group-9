8dba167aa969631cd9c84b0105beb5422cf6594d
Drago361 <alexftvyt@gmail.com>
2024-12-23 17:05:37 +0200

    urls

--- django/django_desks/desk_controller/urls.py
+++ django/django_desks/desk_controller/urls.py
@@ -1,6 +1,1 @@
 from django.urls import path
-from ..django_desks.views import fetch_desks
-
-urlpatterns = [
-    path('fetch-desks/', fetch_desks, name='fetch_desks'),
-]
--- django/django_desks/django_desks/urls.py
+++ django/django_desks/django_desks/urls.py
@@ -4,7 +4,7 @@ from desk_controller.views import fetch_desks, desk_info, desk_state_update
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', fetch_desks, name='fetch_desks'),
-    path('<int:desk_id>/', desk_info, name='desk'),
-    path('<int:desk_id>/state/', desk_state_update, name='desk_category_update'),
+    path('home/', fetch_desks, name='fetch_desks'),
+    path('home/<int:desk_id>/', desk_info, name='desk'),
+    path('home/<int:desk_id>/state/', desk_state_update, name='desk_category_update'),
 ]
