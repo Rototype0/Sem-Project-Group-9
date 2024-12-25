27cd06f63ef2ea969212445ad0e243a6a2491574
Norrokas <rokutis.norbutas@gmail.com>
2024-12-25 23:25:03 +0100

    Created some basic user dashboards for the various users that we inted to have, currently bare bones and having some conflicts due to nt being able to run the web app itslef

--- django/django_desks/home/views.py
+++ django/django_desks/home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from django.contrib.auth.decorators import login_required
+
+from django.http import HttpResponseForbidden
+
+from .models import UserProfile
+
 # Create your views here.
 
 def dashboard(request):
@@ -7,3 +13,16 @@ def dashboard(request):
 
 def about(request):
     return render(request, 'home/about.html')
+
+def dasboard(request):
+
+    user_profile = UserProfile.objects.get(user=request.user)
+
+    if user_profile.role == 'manager':
+        return render(request, 'dashboard/manager_dashboard.html')
+    elif user_profile.role == 'user':
+        return render(request, 'dashboard/user_dashboard.html')
+    elif user_profile.role == 'cleaner':
+        return render(request, 'dashboard/cleaner_dashboard.html')
+    else:
+        return HttpResponseForbidden("Access Denied")
